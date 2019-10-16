3059a52b333e4816a21d39f8845f70e2a8fcaaaf
Nicholas Parks <s3658692@student.rmit.edu.au>
2019-10-16 15:31:41 +1100

    dynamic icon displayed

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var async = require('async');
 var request = require('request');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient
-var app = express();
 
+var app = express();
 app.use(express.static('views/images'));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main' }));
@@ -14,12 +14,12 @@ app.set('view engine', 'handlebars');
 
 app.set('port', (process.env.PORT || 3000));
 
-
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.post('/', urlencodedParser,function(req,res){
     var data = {};
-    var api_key ='RGAPI-0048cdc5-a6eb-4598-8de0-de68c421055d';
+    var api_key ='RGAPI-d707939c-f2c8-4433-ab3b-7287f7ff2fd7';
     var s_toSearch = req.body.name
     var r_toSearch = req.body.region
     var sum_URL = 'https://'+r_toSearch+'.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+s_toSearch+'?api_key=' +api_key;
@@ -50,16 +50,23 @@ app.post('/', urlencodedParser,function(req,res){
                             for (var x in json) {
                                 if (json[x] !== 'undefined') {
                                     if (json[x].queueType === 'RANKED_SOLO_5x5') {
-                                        data.tier = json[x].tier;
-                                        data.rank = json[x].rank;
                                         try {
-                                            var favChamp = await findChampMastery(cm_URL)
-                                            data.champIcon = favChamp.icon
-                                            data.champName = favChamp.name
+                                            data.rank = json[x].rank;
+                                            var nRank = await findRankIcon(json[x].tier);
+                                            data.tier = nRank.Rank;
+                                            data.tierIcon = nRank.Icon;
+                                            try {
+                                                var favChamp = await findChampMastery(cm_URL)
+                                                data.champIcon = favChamp.icon
+                                                data.champName = favChamp.name
 
-                                        }catch(err){
+                                            } catch (err) {
+                                                console.log(err)
+                                            }
+                                        } catch (err) {
                                             console.log(err)
                                         }
+
                                     }
                                 }
                             }
@@ -95,6 +102,22 @@ app.listen(app.get('port'),function(){
     console.log('server started on port '+ app.get("port"))
 });
 
+function findRankIcon(rank) {
+    return new Promise(function (resolve ,reject) {
+        var nRank = upperCaseFirstLetter(lowerCaseAllWordsExceptFirstLetters(rank));
+        console.log(nRank)
+        MongoClient.connect("mongodb+srv://nick:lolman1@cluster0-kxw5r.gcp.mongodb.net/test?retryWrites=true&w=majority", function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("LeagueofME");
+            dbo.collection("Ranks").findOne({'Rank': nRank}, function(err, result) {
+                if (err) throw err;
+                console.log(result)
+                db.close();
+                resolve(result);
+            });
+        });
+    })
+}
 function findChampMastery(cm_URL) {
     return new Promise(function (resolve ,reject) {
         request(cm_URL,  function (err, response, body) {
@@ -119,4 +142,16 @@ function findChampMastery(cm_URL) {
         });
     })
 }
+
+
+function upperCaseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lowerCaseAllWordsExceptFirstLetters(string) {
+    return string.replace(/\w\S*/g, function (word) {
+        return word.charAt(0) + word.slice(1).toLowerCase();
+    });
+}
+
 
