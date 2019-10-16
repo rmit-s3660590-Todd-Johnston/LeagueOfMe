a5f36d0176ec1327855ed86de26ed8cde25eeb1a
Nicholas Parks <s3658692@student.rmit.edu.au>
2019-10-16 12:27:40 +1100

    database setup, favourite champion displayer

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var path = require('path');
 var exphbs = require('express-handlebars');
 var async = require('async');
 var request = require('request');
-var bodyParser = require('body-parser')
-
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
 var app = express();
 
+app.use(express.static('views/images'));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.set('port', (process.env.PORT || 3000));
+
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/', urlencodedParser,function(req,res){
@@ -19,28 +22,44 @@ app.post('/', urlencodedParser,function(req,res){
     var api_key ='RGAPI-65fc5096-6b25-42fc-8515-222d0f15058b';
     var s_toSearch = req.body.name
     var r_toSearch = req.body.region
-    var URL = 'https://'+r_toSearch+'.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+s_toSearch+'?api_key=' +api_key;
-    //https://oc1.api.riotgames.com/lol/league/v4/entries/by-summoner/e5lrPFt6jsk12aEZqkvL4QimZA84t0IaD2z0YZXTxC6S?api_key=RGAPI-3f7edfc8-7ac7-42d2-a35e-8f417560d489
+    var sum_URL = 'https://'+r_toSearch+'.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+s_toSearch+'?api_key=' +api_key;
     async.waterfall([
         function(callback){
-        request(URL, function (err, response, body) {
+        request(sum_URL, async function (err, response, body) {
             if(!err && response.statusCode == 200){
                 var json = JSON.parse(body);
                 data.id = json.id;
                 data.name = json.name;
-                URL = 'https://'+r_toSearch+'.api.riotgames.com/lol/league/v4/entries/by-summoner/'+data.id+'?api_key=' +api_key;
-                request(URL, function (err, response, body) {
+                sum2_URL = 'https://'+r_toSearch+'.api.riotgames.com/lol/league/v4/entries/by-summoner/'+data.id+'?api_key=' +api_key;
+                cm_URL = 'https://'+r_toSearch+'.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'+data.id+'?api_key=' +api_key;
+                request(sum2_URL, async function (err, response, body) {
                     if(!err && response.statusCode == 200){
                         var json = JSON.parse(body);
                         if(json.length === 0 ){
                             data.tier = 'unranked, ';
-                            data.rank = 'cannot find stats';
+                            data.rank = '';
+                            try {
+                                var favChamp = await findChampMastery(cm_URL)
+                                data.champIcon = favChamp.icon
+                                data.champName = favChamp.name
+
+                            }catch(err){
+                                console.log(err)
+                            }
                         }else {
                             for (var x in json) {
                                 if (json[x] !== 'undefined') {
                                     if (json[x].queueType === 'RANKED_SOLO_5x5') {
                                         data.tier = json[x].tier;
                                         data.rank = json[x].rank;
+                                        try {
+                                            var favChamp = await findChampMastery(cm_URL)
+                                            data.champIcon = favChamp.icon
+                                            data.champName = favChamp.name
+
+                                        }catch(err){
+                                            console.log(err)
+                                        }
                                     }
                                 }
                             }
@@ -75,3 +94,29 @@ app.get('/',function(req,res){
 app.listen(app.get('port'),function(){
     console.log('server started on port '+ app.get("port"))
 });
+
+function findChampMastery(cm_URL) {
+    return new Promise(function (resolve ,reject) {
+        request(cm_URL,  function (err, response, body) {
+            if(!err && response.statusCode == 200) {
+                var json = JSON.parse(body);
+                var champID = json[0].championId
+                console.log(champID)
+                MongoClient.connect("mongodb+srv://nick:lolman1@cluster0-kxw5r.gcp.mongodb.net/test?retryWrites=true&w=majority", function(err, db) {
+                    if (err) throw err;
+                    var dbo = db.db("LeagueofME");
+                    dbo.collection("Champions").findOne({'key': champID.toString()}, function(err, result) {
+                        if (err) throw err;
+                        var result = result
+                        db.close();
+                        resolve(result);
+                    });
+                });
+
+
+
+            }
+        });
+    })
+}
+
